5b93356537078fe9e3716f2621e5b80839621371
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-09-24 17:17:33 -0700

    upto finalize

---
+++ W6D2/ActiveRecord Lite/lib/01_sql_object.rb
@@ -0,0 +1,69 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    @columns ||= DBConnection.execute2(<<-SQL)
+      SELECT 
+        *
+      FROM 
+        #{self.table_name}
+      LIMIT 
+        1
+    SQL
+    @columns[0].map(&:to_sym)
+  end
+
+  def self.finalize!
+    @columns.each do |col|
+      puts col
+    end
+  end
+
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
