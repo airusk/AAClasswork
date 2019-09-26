8afbf17553c00e8c27bf7b0399472fec3e3916bc
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-09-26 10:17:52 -0700

    artworkshare project migrations added not migrated yet

--- W6D3/artworkshare/app/models/artwork.rb
+++ W6D3/artworkshare/app/models/artwork.rb
@@ -1,21 +1,2 @@
-# == Schema Information
-#
-# Table name: artworks
-#
-#  id         :bigint           not null, primary key
-#  title      :string           not null
-#  image_url  :string           not null
-#  artist_id  :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Artwork < ApplicationRecord
-  validates :artist_id, uniqueness: { scope: :title}
-
-  belongs_to :artist,
-    foreign_key: :artist_id,
-    class_name: :User
-
-  
 end
--- W6D3/artworkshare/db/migrate/20190926163008_create_users.rb
+++ W6D3/artworkshare/db/migrate/20190926163008_create_users.rb
@@ -1,8 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+    # add_column :table_name, :column_name, :type, options_hash
+    # remove_column :table_name, :column_name
+    # rename_column :table_name, :old_column_name, :new_column_name
+    # rename_table :old_table_name, :new_table_name
+    # add_index :table_name, [:column1, :column2], options_hash
+    # change_column :table_name, :column_name, :type, options_hash
     create_table :users do |t|
-
+      t.string :username, null: false
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
   end
 end
