40631ea0fef349529c556bc2c71795121a6ad549
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-10-30 11:16:44 -0700

    front_end_auth complete

---
+++ W11D3/front_end_auth/frontend/bluebird.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createStore from './store/store';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  let preloadedState = undefined; //this is to make sure that we have a variable taht we can use
+  if (window.currentUser) {
+    preloadedState = { // preloaded state mimics the shape of our state
+      session: {
+        currentUser: window.currentUser
+      }
+    };
+  }
+  const store = createStore(preloadedState);
+  // const store = createStore();
+
+  ReactDOM.render(<Root store={store} />, root);
+})
